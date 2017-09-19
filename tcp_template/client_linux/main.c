9dafaaff4c1ab7c97cd2da7909570a6bf0f017d6
Masha <volkova.maria.d@gmail.com>
2017-09-19 14:16:43 +0300

    djf

--- tcp_template/client_linux/main.c
+++ tcp_template/client_linux/main.c
@@ -7,72 +7,90 @@
 
 #include <string.h>
 
+char* readn(int sockfd, char* bytebuffer, int datalen) {
+    for (int i = 0; i < datalen; i++) {
+        if (read(sockfd, bytebuffer + i, 1) < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+        
+    }
+    return bytebuffer;
+}
+
 int main(int argc, char *argv[]) {
-    int sockfd, n;
+    argv[1] = "localhost";
+    argv[2] = "5001";
+    int sockfd;
+    int n;
     uint16_t portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
+    
     char buffer[256];
-
+    
+    
+    
+    
     if (argc < 3) {
-        fprintf(stderr, "usage %s hostname port\n", argv[0]);
-        exit(0);
+      fprintf(stderr, "usage %s hostname port\n", argv[0]);
+      exit(0);
     }
-
+    
     portno = (uint16_t) atoi(argv[2]);
-
+    
     /* Create a socket point */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
+    
     if (sockfd < 0) {
+        printf("1");
         perror("ERROR opening socket");
         exit(1);
     }
-
+    
     server = gethostbyname(argv[1]);
-
+    
     if (server == NULL) {
+        printf("2");
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    
+    bzero((char *) &serv_addr, sizeof (serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy(server->h_addr, (char *) &serv_addr.sin_addr.s_addr, (size_t) server->h_length);
     serv_addr.sin_port = htons(portno);
-
+    
     /* Now connect to the server */
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)) < 0) {
+        printf("3");
         perror("ERROR connecting");
         exit(1);
     }
-
+    
     /* Now ask for a message from the user, this message
-       * will be read by server
-    */
-
+     * will be read by server
+     */
+    
     printf("Please enter the message: ");
     bzero(buffer, 256);
     fgets(buffer, 255, stdin);
-
+    
     /* Send message to the server */
     n = write(sockfd, buffer, strlen(buffer));
-
+    
     if (n < 0) {
+        printf("4");
         perror("ERROR writing to socket");
         exit(1);
     }
-
+    
     /* Now read server response */
     bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-
-    if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-
-    printf("%s\n", buffer);
+    printf("%s\n", readn(sockfd, buffer, 1));
+    
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
+    
     return 0;
 }
